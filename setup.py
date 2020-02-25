dd38ac110ac2eaee5265269bf2c2386446704bb6
mmasinas <myra.masinas@utoronto.ca>
2020-02-25 11:13:12 -0500

    New version, sigma=1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ ext_modules = [
 setup(
     name="segmentation_phenix",
     packages=["segmentation_phenix"],
-    version='0.1.0',
+    version='0.1.1',
     description='Mixture model segmentation for Phenix images',
     author='Matej Usaj',
     author_email='m.usaj@utoronto.ca',
