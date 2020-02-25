3efeaadb3a58bb2409be8eec58bd4b10d4e43ecf
mmasinas <myra.masinas@utoronto.ca>
2020-02-25 09:49:07 -0500

    rename to segmentation_phenix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, Extension
 our_flags = ["-march=native", "-std=c++11", "-DNDEBUG", "-Ofast", "-Wcpp"]
 
 ext_modules = [
-    Extension("segmentation.segmentation", ["segmentation/segmentation.pyx", ],
+    Extension("segmentation_phenix.segmentation", ["segmentation_phenix/segmentation.pyx", ],
               language="c++",
               include_dirs=[np.get_include(), sysconfig.get_config_var('INCLUDEDIR')],
               extra_compile_args=our_flags,
@@ -19,11 +19,11 @@ setup(
     name="segmentation_phenix",
     packages=["segmentation_phenix"],
     version='0.1.0',
-    description='Mixture model segmentation',
+    description='Mixture model segmentation for Phenix images',
     author='Matej Usaj',
     author_email='m.usaj@utoronto.ca',
-    url='https://github.com/mmasinas/segmentation',
-    download_url='https://github.com/mmasinas/segmentation/archive/master.zip',
+    url='https://github.com/mmasinas/segmentation_phenix',
+    download_url='https://github.com/mmasinas/segmentation_phenix/archive/master.zip',
     keywords=['mixture', 'model', 'segmentation'],
     classifiers=[],
     cmdclass={"build_ext": build_ext},
